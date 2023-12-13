efe045147e57e8823ac2acca9fe2b634f64fd1d2
edakizak <edakizak.de@gmail.com>
2023-12-13 11:25:28 +0100

    Added booleans challenge

--- js/js-conditions-and-booleans_booleans/js/index.js
+++ js/js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
-console.log(Boolean("Try it out"));
+result = 2 == "2"; //true
+result = 2 === "2"; //false
+result = 2 === 2; //true
+result = "hello world"; //true
+result = ""; //false
+result = true; // true
+result = 0; // false
+result = -50; // true
+result = 50; // true
+result = 5 > 1; // true
+result = "1 > 10"; // true
+result = "false"; //true
+
+console.log(Boolean(""));
