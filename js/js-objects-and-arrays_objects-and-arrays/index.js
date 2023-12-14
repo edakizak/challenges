555bba94e146986bdec2a4c8ca39931ebca13420
edakizak <edakizak.de@gmail.com>
2023-12-14 14:17:04 +0100

    Fixed challenge

--- js/js-objects-and-arrays_objects-and-arrays/index.js
+++ js/js-objects-and-arrays_objects-and-arrays/index.js
@@ -50,27 +50,27 @@ const people = [
 // EXERCISE 1
 // Change the value of the variable nameOfFirstPerson to equal the name of the first person.
 
-const nameOfFirstPerson = "homer";
+const nameOfFirstPerson = people[0].name;
 
 // EXERCISE 2
 // Change the value of the variable homersOtherFriends to equal the other friends of Homer.
 
-const homersOtherFriends = ["Marge", "Bart", "Lisa"];
+const homersOtherFriends = people[0]["other friends"];
 
 // EXERCISE 3
 // Change the value of the variable ageOfThirdPerson to equal the age of the third person.
 
-const ageOfThirdPerson = 10;
+const ageOfThirdPerson = person[2].age;
 
 // EXERCISE 4
 // Change the value of the variable mothersNameOfSecondPerson to equal the mothers name of the second person.
 
-const mothersNameOfSecondPerson = "Jacqueline Bouvier";
+const mothersNameOfSecondPerson = person[1].parents.mothersName;
 
 // EXERCISE 5
 // Change the value of the variable animaltypeOfLisasPet to equal the type of animal of Lisas pet.
 
-const animaltypeOfLisasPet = "Santa's Little Helper";
+const animaltypeOfLisasPet = person[3].pet.name;
 
 export {
   nameOfFirstPerson,
