64ce3a6c4d0696f60d229be8f5a31c2a2bae19f5
edakizak <edakizak.de@gmail.com>
2024-01-04 15:20:49 +0100

    Space Rocket Challenge PR

--- js/js-structure_spacerocket/js/launchSequence.js
+++ js/js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,21 @@
 // Implement the launch sequence function here and export it as the default export.
+
+import { loadPayload } from "../js/core/load.js";
+import { fuel } from "../js/core/fuel.js";
+import { countdown } from "../js/core/countdown.js";
+import { liftoff } from "../js/core/liftoff.js";
+import { deployPayload } from "../js/core/deploy.js";
+import { FISHSAT, NFSAT } from "../js/payload/satellites.js";
+
+export default function launch() {
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+  fuel();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  liftoff();
+  deployPayload();
+}
