aba32705e6bd88ecbdd103ffa1e4f65f605c6376
edakizak <edakizak.de@gmail.com>
2024-01-05 14:16:18 +0100

    fixed all the challenges

---
+++ js/js-structure_shapes/index.js
@@ -0,0 +1,15 @@
+import getRandomColor from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
+import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
+console.clear();
+
+const root = document.getElementById("root");
+
+const circle = Circle(getRandomColor);
+
+const square = Square(getRandomColor);
+
+const pentagon = Pentagon(getRandomColor);
+
+root.append(circle, square, pentagon);
---
+++ js/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,31 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const postTitle = "Welcome";
+const postText = "What's Up";
+const postLikes = 50;
+const postCreator = "eda";
+const isReported = true;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(postTitle);
+console.log(postText);
+console.log(postLikes + 1);
+console.log(postCreator);
+console.log(isReported);
+// --^-- write your code here --^--
