6a36866e4edd38b78670e23c07e615774ad53db1
edakizak <edakizak.de@gmail.com>
2024-01-08 15:19:48 +0100

    download challenge

--- js/js-structure_shapes/components/Pentagon/Pentagon.js
+++
@@ -1,8 +0,0 @@
-export default function Pentagon(getRandomColor) {
-  const pentagon = document.createElement("div");
-  pentagon.classList.add("pentagon");
-  pentagon.addEventListener("click", () => {
-    pentagon.style.backgroundColor = getRandomColor();
-  });
-  return pentagon;
-}
--- js/js-structure_shapes/components/Square/Square.js
+++
@@ -1,8 +0,0 @@
-export default function Square(getRandomColor) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("click", () => {
-    square.style.backgroundColor = getRandomColor();
-  });
-  return square;
-}
