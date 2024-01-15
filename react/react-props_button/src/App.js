a15b38717ea87ef2c69308df9688374c1a6f020c
edakizak <edakizak.de@gmail.com>
2024-01-15 17:33:40 +0100

    add onClick and finish challenge

--- react/react-props_button/src/App.js
+++ react/react-props_button/src/App.js
@@ -2,17 +2,24 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function handleClick() {
+    alert("Clicked!");
+  }
   return (
     <div>
-      <Button color="blue" text="click me" />
+      <Button color="blue" text="click me" onClick={handleClick} />
     </div>
   );
 }
 
-function Button({ color, disabled, text }) {
+function Button({ color, disabled, text, onClick }) {
   return (
     <div>
-      <button style={{ backgroundColor: color }} disabled={disabled}>
+      <button
+        style={{ backgroundColor: color, height: "100px" }}
+        disabled={disabled}
+        onClick={onClick}
+      >
         {text}
       </button>
     </div>
