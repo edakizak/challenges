b3b3b69e0ddec5855f0cc659c7e130cc76dc54d3
edakizak <edakizak.de@gmail.com>
2024-01-17 15:39:16 +0100

    react arrays fruits challenge

--- react/react-with-arrays_fruits/src/App.js
+++ react/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,39 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
-
+  const fruits = [
+    {
+      id: 1,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 2,
+      name: "🥥 Coconat",
+      color: "red",
+    },
+    {
+      id: 3,
+      name: "🥝 Kiwi",
+      color: "green",
+    },
+    {
+      id: 4,
+      name: "🍋 Lemon",
+      color: "blue",
+    },
+    {
+      id: 5,
+      name: "🍇 Grape",
+      color: "pink",
+    },
+  ];
+  console.log(fruits);
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <Card key={fruit.id} name={fruit.name} color={fruit.color} />
+      ))}
     </div>
   );
 }
