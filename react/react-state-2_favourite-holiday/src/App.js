3304c15ad9be6001d81bd1f1e7bf09cbcd9bca1a
edakizak <edakizak.de@gmail.com>
2024-01-18 12:52:20 +0100

    favourite holiday challenge

--- react/react-state-2_favourite-holiday/src/App.js
+++ react/react-state-2_favourite-holiday/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
   }
@@ -21,19 +25,25 @@ export default function App() {
           type="text"
           name="holiday"
           placeholder="e.g. Christmas"
+          onChange={(holiday) => setHoliday(holiday.target.value)}
         />
         <label htmlFor="date">Date: </label>
-        <input id="date" type="date" name="date" />
+        <input
+          id="date"
+          type="date"
+          name="date"
+          onChange={(date) => setDate(date.target.value)}
+        />
         <button type="submit" className="form__submit-button">
           Submit
         </button>
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
