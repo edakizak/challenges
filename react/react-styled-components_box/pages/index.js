da1211f9ddc37e43c04ad40411f7a5d67a49a719
edakizak <edakizak.de@gmail.com>
2024-01-26 11:24:15 +0100

    react styled components box challenge

--- react/react-styled-components_box/pages/index.js
+++ react/react-styled-components_box/pages/index.js
@@ -1,10 +1,19 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "../components/BoxWithStyledComponents.js";
+// import { View } from "react";
+import React from "react";
 
 export default function HomePage() {
   return (
-    <div>
+    // ### Bonus: Aligning Boxes Horizontally
+    <div
+      style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+    >
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+
+      <BoxWithStyledComponents $isBlack={false} />
+      <BoxWithStyledComponents $isBlack={true} />
     </div>
   );
 }
