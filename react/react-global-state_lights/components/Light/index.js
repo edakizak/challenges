be8d1ab40234affaba3511c931f8b09d808d1c1d
edakizak <edakizak.de@gmail.com>
2024-01-29 13:33:29 +0100

    react global state lights challenge

--- react/react-global-state_lights/components/Light/index.js
+++ react/react-global-state_lights/components/Light/index.js
@@ -1,25 +1,16 @@
-import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
-export default function Light({ name }) {
-  const [isOn, setIsOn] = useState(false);
-
+export default function Light({ light, onToggle }) {
   function handleToggle() {
-    setIsOn((isOn) => !isOn);
+    onToggle(light.id);
   }
 
   return (
-    <LightButton
-      type="button"
-      onClick={() => {
-        handleToggle();
-      }}
-      $isOn={isOn}
-    >
-      <Icon $isOn={isOn}>💡</Icon>
+    <LightButton type="button" onClick={handleToggle} isOn={light.isOn}>
+      <Icon isOn={light.isOn}>💡</Icon>
       <Text>
-        <Name>{name}</Name>
-        <State>{isOn ? "On" : "Off"}</State>
+        <Name>{light.name}</Name>
+        <State>{light.isOn ? "On" : "Off"}</State>
       </Text>
     </LightButton>
   );
